72d71f74c0fa3606dc5d752faf078217e635bac4
dailykaran <dinakaran83@gmail.com>
2026-09-03 20:40:38 +0530

    headless true now

--- tests/global_auth/global-setup.ts
+++ tests/global_auth/global-setup.ts
@@ -255,7 +255,7 @@ async function globalSetup(_config: FullConfig): Promise<void> {
   });
   fs.mkdirSync(AUTH_DIR, { recursive: true });
 
-  const browser = await chromium.launch({ headless: false, args: ['--no-sandbox']});
+  const browser = await chromium.launch({ headless: true, args: ['--no-sandbox']});
 
   try {
     const selectedRole = process.env.PLAYWRIGHT_ROLE;
